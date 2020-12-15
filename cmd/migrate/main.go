f858ef962045cb002c00fcb5ad7e119da25076a8
Luan Guimarães <luang@riseup.net>
2020-12-15 02:03:51 -0300

    feat(migrations): configuring user migrations and dockerfiles

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -2,8 +2,47 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func getEnv(key, fallback string) string {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		value = fallback
+	}
+	return value
+}
 
 func main() {
-	fmt.Println("ARNALDO")
+	pgUsername := getEnv("POSTGRES_USER", "freegrow")
+	pgPassword := getEnv("POSTGRES_PASSWORD", "freegrow")
+	pgDatabase := getEnv("POSTGRES_DATABASE", "freegrow")
+	pgHost := getEnv("POSTGRES_HOST", "localhost")
+	pgPort := getEnv("POSTGRES_PORT", "5432")
+
+	m, err := migrate.New(
+		"file://db/migrations",
+		fmt.Sprintf(
+			"postgres://%s:%s@%s:%s/%s?sslmode=disable",
+			pgUsername,
+			pgPassword,
+			pgHost,
+			pgPort,
+			pgDatabase,
+		),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := m.Up(); err != nil {
+		log.Fatal(err)
+	}
 }
